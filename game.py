55b9815652433bc6c74c2c7b3776597670773970
TheEmpressDiadema <mauhkpaffftep@gmail.com>
2025-02-05 13:40:21 +0300

    я сосал

--- game.py
+++ game.py
@@ -7,31 +7,32 @@ from validators import IngameValidator
 
 class Game:
 
-    def setup(self, words: list[str]):
+    def __init__(self, words: list[str]):
         self._word = Word(random.choice(words))
         self._gallows = Gallows()
         self._used_letters = []
         self._validator = IngameValidator()
         self._stage = 0
+        self._max_stages = 5
 
     def run(self):
         while True:
             self._run_round()
             if self._word.is_fully_revealed():
-                print("Вы выиграли\n")
+                print("Вы выиграли")
                 print(f"Загаданное слово: {self._word.word}")
                 break
-            if self._stage == 7:
+            if self._stage == self._max_stages:
                 self._gallows.print_stage(self._stage)
-                print("Вы проиграли\n")
+                print("Вы проиграли")
                 print(f"Загаданное слово: {self._word.word}")
                 break
 
     def _print_status(self):
         self._gallows.print_stage(self._stage)
         print(f"Состояние отгадвания =) {self._word.state}")
-        print(f"Использованные буквы: {','.join(self._used_letters)}\n")
-        print(f"Осталось попыток: {self._stage}\n")
+        print(f"Использованные буквы: {','.join(self._used_letters)}")
+        print(f"Осталось попыток: {self._stage}")
 
     def _validate_input(self, user_input: str) -> bool:
         is_valid = self._validator.validate_alpha(user_input)
