522a30cb5f342eb244820b674523b69d2b66cdb4
TheEmpressDiadema <mauhkpaffftep@gmail.com>
2025-02-05 16:20:41 +0300

    da

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ from manager import GameManager
 from menu import Menu, DifficultyMenu
 
 class Program:
-    
+
     def __init__(self):
         self._menu_view = Menu()
         self._difficulty_view = DifficultyMenu()
+        self._manager = GameManager()
 
     def run(self):
-        manager = GameManager()
         while True:
             self._menu_view.print()
             match input():
                 case MenuElements.START.value:
-                    manager.run_game()
+                    self._manager.run_game()
                 case MenuElements.DIFFICULTY.value:
                     self._difficulty_view.print()
-                    manager.set_difficulty()
+                    self._manager.set_difficulty()
                 case MenuElements.QUIT.value:
                     break
                 case _:
